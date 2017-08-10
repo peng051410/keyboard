c997c5d3319f1a581dfe1f55fe8eed3d65eae6db
peng051410 <peng051410@126.com>
2017-08-10 19:41:02 +0800

    增加映射

--- hammerspoon/delete-words.lua
+++ hammerspoon/delete-words.lua
@@ -52,7 +52,7 @@ hs.hotkey.bind({'ctrl'}, ';', function()
   -- then delete the selected text.
   if isInTerminal() then
     hotkeyForControlK = hs.fnutils.find(hs.hotkey.getHotkeys(), function(hotkey)
-      return hotkey.idx == '^k'
+      return hotkey.idx == '^K'
     end)
     if hotkeyForControlK then hotkeyForControlK:disable() end
 
